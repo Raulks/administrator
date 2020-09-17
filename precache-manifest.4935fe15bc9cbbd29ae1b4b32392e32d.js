42df2f15ed8813a2e5ca0be723e6f65ec92c51c0
Raulks <rksole90@gmail.com>
2020-09-17 16:53:30 +0200

    Updates

---
+++ precache-manifest.4935fe15bc9cbbd29ae1b4b32392e32d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a7cbece925c95378dafbc368da9dbf",
+    "url": "/administrator/index.html"
+  },
+  {
+    "revision": "7026a860cefb0556c6ad",
+    "url": "/administrator/static/css/main.6b10cd34.chunk.css"
+  },
+  {
+    "revision": "3389c54ecd56bf69075a",
+    "url": "/administrator/static/js/2.c2129fa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/administrator/static/js/2.c2129fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7026a860cefb0556c6ad",
+    "url": "/administrator/static/js/main.e9f79c75.chunk.js"
+  },
+  {
+    "revision": "a060ba83ad21352e6165",
+    "url": "/administrator/static/js/runtime-main.c5169ef8.js"
+  }
+]);
